6d67f1cba238c0639679ca6c1048c2e3f93eddda
NeilSCGH <neil.segard@gmail.com>
2020-05-02 03:42:31 +0200

    output folder added, fix bug in file path root

--- main.py
+++ main.py
@@ -3,52 +3,64 @@ import os
 from lib.tools import *
 
 class program():
-	def __init__(self,args):
-	    self.tool = tools(args)
-	    self.setup(args)
+    def __init__(self,args):
+        self.tool = tools(args)
+        self.setup(args)
 
-	def setup(self,args):
-	    if self.tool.argHasValue("-f"):#the folder for source files
-	      val=self.tool.argValue("-f")
-	      self.folderPath=val.replace("\\","/")
-	      print("folderPath",self.folderPath)
-	    else:
-	      self.stop("Error, -f is missing !")
+    def setup(self,args):
+        if self.tool.argHasValue("-sf"):#the folder for source files
+          val=self.tool.argValue("-sf")
+          self.folderPathSource=val.replace("\\","/")
+          #print("folderPathSource",self.folderPathSource)
+        else:
+          self.stop("Error, -sf (source fold) is missing !")
 
-	def run(self):
-		for root, dirs, files in os.walk(self.folderPath):
-		    for filename in files:
-		    	try:
-		    		self.move(filename)
-		    	except:1
-	def move(self,file):
-		year=file[:4]
-		month=file[4:6]
+        if self.tool.argHasValue("-of"):#the folder for source files
+          val=self.tool.argValue("-of")
+          self.folderPathOutput=val.replace("\\","/")
+          try:
+            os.mkdir(self.folderPathOutput,0o777)
+          except:1
+          #print("folderPathOutput",self.folderPathOutput)
+        else:
+          self.stop("Error, -of (output folder) is missing !")
 
-		if "2000"<= year and year<="2030" and "01"<=month and month<="12":
-			print("OK",file)
-			previousPath=self.folderPath + "/" + file
-			newFolderPath=self.folderPath + "/" + year
-			try:
-				os.mkdir(newFolderPath,0o777)
-			except:1
-			newFolderPath += "/" + month + "/" 
-			try:
-				os.mkdir(newFolderPath,0o777)
-			except:1
+    def run(self):
+        for root, dirs, files in os.walk(self.folderPathSource):
+            for filename in files:
+                #try:
+                self.move(root,filename)
+                #except:1
+    def move(self,rootPath,file):
+        year=file[:4]
+        month=file[4:6]
 
-			newPath=newFolderPath + file
+        if "2000"<= year and year<="2030" and "01"<=month and month<="12":
+            print("OK",file)
+            previousPath=rootPath + "/" + file
 
-			os.rename(previousPath, newPath)
-		else:
-			print("NO",file)
+            newFolderPath=self.folderPathOutput + "/" + year
+            try:
+                os.mkdir(newFolderPath,0o777)
+            except:1
+
+            newFolderPath += "/" + month + "/" 
+            try:
+                os.mkdir(newFolderPath,0o777)
+            except:1
+
+            newPath=newFolderPath + file
+            
+            os.rename(previousPath, newPath)
+        else:
+            print("NO",file)
 
 
-	def stop(self,msg):
-		print(msg)
-		exit(0)
+    def stop(self,msg):
+        print(msg)
+        exit(0)
 
 
 if __name__ == '__main__':
-	prog=program(sys.argv)
-	prog.run()
+    prog=program(sys.argv)
+    prog.run()
